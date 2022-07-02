f2adbf2bd18cf18d30be76cd707545b13264f4f5
Gabriel Fonseca <gdfonseca@leroymerlin.com.br>
2022-07-02 10:55:22 -0300

    feat(github): add phpcs gui exception

---
+++ tests/Unit/src/Presenters/CLI/FetchGitHubInfoTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fnsc\Presenters\CLI;
+
+use PHPUnit\Framework\TestCase;
+
+class FetchGitHubInfoTest extends TestCase
+{
+    public function testShouldHandle(): void
+    {
+        // Set
+
+
+        // Expectations
+
+        // Actions
+
+        // Assertions
+    }
+}
