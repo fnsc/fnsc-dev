329ecd1faefa694fb4a391db44b2b3367eff1f6f
Gabriel Fonseca <gdfonseca@leroymerlin.com.br>
2022-07-04 22:17:10 -0300

    feat(home): add footer

--- src/Presenters/GUI/home/index.blade.php
+++ src/Presenters/GUI/home/index.blade.php
@@ -3,7 +3,7 @@
     {{ $location }}
 @endsection
 @section('content')
-    <div class="container">
+    <div class="container custom-container d-flex flex-column">
         <div class="row">
             <div class="col-12 avatar">
                 <img src="{{ secure_asset($user['avatarUrl']) }}" alt="avatar" class="avatar-img img-fluid mx-auto d-block">
@@ -24,5 +24,19 @@
                 @endforeach
             </div>
         </div>
+        <footer class="row mt-auto p-3">
+            <div class="col-12 text-center">
+                <span>
+                    Made with
+                    <svg class="img-fluid heart">
+                        <use xlink:href="{{ asset('img/heart.svg') }}#heart"/>
+                    </svg>
+                    using
+                    <a href="https://laravel.com">Laravel</a>.
+                    See this project on
+                    <a href="https://github.com/fnsc/fnsc-dev">GitHub</a>.
+                </span>
+            </div>
+        </footer>
     </div>
 @endsection
