248961983dee929bdcd69019a20c286ce9057249
Gabriel Fonseca <gdfonseca@leroymerlin.com.br>
2022-07-11 22:14:08 -0300

    feat(assets): secure url assets generator

--- src/Infra/Adapters/UrlGenerator.php
+++ src/Infra/Adapters/UrlGenerator.php
@@ -2,18 +2,24 @@
 
 namespace Fnsc\Infra\Adapters;
 
+use Fnsc\Application\Contracts\Config as ConfigContract;
 use Fnsc\Application\Contracts\UrlGenerator as UrlGeneratorContract;
 use Illuminate\Contracts\Routing\UrlGenerator as IlluminateUrlGenerator;
 
 class UrlGenerator implements UrlGeneratorContract
 {
+    private const LOCAL_ENVIRONMENT = 'local';
+
     public function __construct(
-        private readonly IlluminateUrlGenerator $generator
+        private readonly IlluminateUrlGenerator $generator,
+        private readonly ConfigContract $config,
     ) {
     }
 
     public function asset(string $url, string $query = ''): string
     {
-        return $this->generator->asset($url) . $query;
+        return self::LOCAL_ENVIRONMENT === $this->config->get('app.env')
+            ? $this->generator->asset($url) . $query
+            : $this->generator->asset($url, true) . $query;
     }
 }
--- tests/Unit/src/Infra/Adapters/UrlGeneratorTest.php
+++ tests/Unit/src/Infra/Adapters/UrlGeneratorTest.php
@@ -2,6 +2,7 @@
 
 namespace Fnsc\Infra\Adapters;
 
+use Fnsc\Application\Contracts\Config as ConfigContract;
 use Illuminate\Contracts\Routing\UrlGenerator as IlluminateUrlGenerator;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
@@ -12,10 +13,16 @@ class UrlGeneratorTest extends TestCase
     {
         // Set
         $generator = m::mock(IlluminateUrlGenerator::class);
+        $config = m::mock(ConfigContract::class);
         /** @phpstan-ignore-next-line  */
-        $urlGenerator = new UrlGenerator($generator);
+        $urlGenerator = new UrlGenerator($generator, $config);
 
         // Expectations
+        /** @phpstan-ignore-next-line  */
+        $config->expects()
+            ->get('app.env')
+            ->andReturn('local');
+
         /** @phpstan-ignore-next-line  */
         $generator->expects()
             ->asset('img/orkut.svg#orkut')
@@ -27,6 +34,35 @@ class UrlGeneratorTest extends TestCase
         // Assertions
         $this->assertSame(
             'http://localhost:8080/img/orkut.svg#orkut',
+            $result
+        );
+    }
+
+    public function testShouldGetProductionAssetUrl(): void
+    {
+        // Set
+        $generator = m::mock(IlluminateUrlGenerator::class);
+        $config = m::mock(ConfigContract::class);
+        /** @phpstan-ignore-next-line  */
+        $urlGenerator = new UrlGenerator($generator, $config);
+
+        // Expectations
+        /** @phpstan-ignore-next-line  */
+        $config->expects()
+            ->get('app.env')
+            ->andReturn('production');
+
+        /** @phpstan-ignore-next-line  */
+        $generator->expects()
+            ->asset('img/orkut.svg#orkut', true)
+            ->andReturn('https://localhost:8080/img/orkut.svg#orkut');
+
+        // Actions
+        $result = $urlGenerator->asset('img/orkut.svg#orkut');
+
+        // Assertions
+        $this->assertSame(
+            'https://localhost:8080/img/orkut.svg#orkut',
             $result
         );
     }
